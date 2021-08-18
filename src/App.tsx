ae3ce05475bcdad4c60de3b23c8e36ae8d4a84e7
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-08-18 08:39:00 +0300

    Changed app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>CRA project example</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +18,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
