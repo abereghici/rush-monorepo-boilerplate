ae0dd048dd1c1785c939b420ddc4970267c1d20f
Alexandru Bereghici <abereghici@tripadvisor.com>
2021-08-19 21:42:35 +0300

    Renamed app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
@@ -7,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CRA project example</p>
         <a
           className="App-link"
           href="https://reactjs.org"
